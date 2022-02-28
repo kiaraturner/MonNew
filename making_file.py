61540a1b86e36bcd3c38e10c3ecbc5242560dd67
kiaraturner <kiaraturner241204@gmail.com>
2022-02-28 10:20:04 +0800

    linear search i think

--- making_file.py
+++ making_file.py
@@ -1,6 +1,6 @@
+"""
 
-
-content = "this ais a bunch of content."
+content = "this is a bunch of content."
 
 content += "\nHere is some content."
 
@@ -16,3 +16,17 @@ file.close()
 file = open("test.txt","a")
 file.write("this is some extra stuff that has been added")
 file.close()
+"""
+
+collection = ["a","b","c","d","e","f","g"]
+
+def linear_search(search_term):
+    for x in collection:
+        if (x == str(search_term)):
+            return True
+        return False
+
+print(linear_search("z"))
+    
+
+
